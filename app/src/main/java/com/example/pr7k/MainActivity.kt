f59abe93989d027468f5dce58ce12c66930e93da
Ferr <ursusmartimus007@gmail.com>
2023-09-22 12:09:01 +0300

    all

--- app/src/main/java/com/example/pr7k/MainActivity.kt
+++ app/src/main/java/com/example/pr7k/MainActivity.kt
@@ -3,16 +3,37 @@ package com.example.pr7k
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.pr7k.MainActivity.TextStyles.testTextStyle
 import com.example.pr7k.ui.theme.PR7KTheme
 
 class MainActivity : ComponentActivity() {
+
+    object TextStyles {
+        val testTextStyle = TextStyle(
+            fontSize = 24.sp,
+            fontFamily = FontFamily.SansSerif,
+            fontWeight = FontWeight.Light,
+            color = Color.White
+        )
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -21,8 +42,15 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
+
                 ) {
-                    Greeting("Android")
+                    Column (
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ){
+                        Greeting()
+                    }
+
                 }
             }
         }
@@ -30,10 +58,11 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Sergey Shillo IKBO-25-21",
+        modifier = modifier,
+        style = testTextStyle
     )
 }
 
@@ -41,6 +70,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     PR7KTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
